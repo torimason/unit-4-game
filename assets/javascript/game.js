d1bbfb9f25c7a521e9beeaef602f3c883424b4b6
Tori Mason <tori.mason@sbcglobal.net>
2018-06-30 13:47:09 -0500

    dried code, kind of

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -9,36 +9,42 @@ var losses = 0;
 
 $("#goal-score-text").text(goalNumber);
 
+var reset = function(){
+    crystalOne = Math.floor(Math.random() * 12) + 1;
+    crystalTwo = Math.floor(Math.random() * 12) + 1;
+    crystalThree = Math.floor(Math.random() * 12) + 1;
+    crystalFour = Math.floor(Math.random() * 12) + 1;
+    goalNumber = Math.floor(Math.random() * 101) + 19;
+    playerNumber = 0;
+    $("#player-score-var").text(playerNumber);
+    $("#goal-score-text").text(goalNumber);
+};
+
+
+var winner = function (){
+    wins++;
+    alert("You WON!");
+    $("#wins-var").text(wins);
+    reset();
+}
+
+var loser = function(){
+    losses++;
+    alert("You LOSE!")
+    $("#losses-var").text(losses);
+    reset();
+}
+
 
 $("#crystal-one").on("click" , function(){
    playerNumber +=crystalOne;
    console.log(playerNumber)
    $("#player-score-var").text(playerNumber)
    if(playerNumber === goalNumber) {
-       wins++
-       $("#wins-var").text(wins);
-       alert("You WON!");
-       crystalOne = Math.floor(Math.random() * 12) + 1;
-       crystalTwo = Math.floor(Math.random() * 12) + 1;
-       crystalThree = Math.floor(Math.random() * 12) + 1;
-       crystalFour = Math.floor(Math.random() * 12) + 1;
-       goalNumber = Math.floor(Math.random() * 101) + 19;
-       playerNumber = 0;
-       $("#player-score-var").text(playerNumber);
-       $("#goal-score-text").text(goalNumber);
+        winner();
    }
    else if(playerNumber > goalNumber) {
-       losses++
-       $("#losses-var").text(losses);
-       alert("you LOSE!");
-       crystalOne = Math.floor(Math.random() * 12) + 1;
-       crystalTwo = Math.floor(Math.random() * 12) + 1;
-       crystalThree = Math.floor(Math.random() * 12) + 1;
-       crystalFour = Math.floor(Math.random() * 12) + 1;
-       goalNumber = Math.floor(Math.random() * 101) + 19;
-       playerNumber = 0;
-       $("#player-score-var").text(playerNumber);
-       $("#goal-score-text").text(goalNumber);
+        loser();
    }
 })
 
@@ -47,30 +53,10 @@ $("#crystal-two").on("click" , function(){
     console.log(playerNumber)
     $("#player-score-var").text(playerNumber)
     if(playerNumber === goalNumber) {
-        wins++
-        $("#wins-var").text(wins);
-        alert("You WON!");
-        crystalOne = Math.floor(Math.random() * 12) + 1;
-        crystalTwo = Math.floor(Math.random() * 12) + 1;
-        crystalThree = Math.floor(Math.random() * 12) + 1;
-        crystalFour = Math.floor(Math.random() * 12) + 1;
-        goalNumber = Math.floor(Math.random() * 101) + 19;
-        playerNumber = 0;
-        $("#player-score-var").text(playerNumber);
-        $("#goal-score-text").text(goalNumber);
+        winner();
     }
     else if(playerNumber > goalNumber) {
-        losses++
-        $("#losses-var").text(losses);
-        alert("you LOSE!");
-        crystalOne = Math.floor(Math.random() * 12) + 1;
-        crystalTwo = Math.floor(Math.random() * 12) + 1;
-        crystalThree = Math.floor(Math.random() * 12) + 1;
-        crystalFour = Math.floor(Math.random() * 12) + 1;
-        goalNumber = Math.floor(Math.random() * 101) + 19;
-        playerNumber = 0;
-        $("#player-score-var").text(playerNumber);
-        $("#goal-score-text").text(goalNumber);
+        loser();
     }
  })
 
@@ -79,30 +65,10 @@ $("#crystal-two").on("click" , function(){
     console.log(playerNumber)
     $("#player-score-var").text(playerNumber)
     if(playerNumber === goalNumber) {
-        wins++
-        $("#wins-var").text(wins);
-        alert("You WON!");
-        crystalOne = Math.floor(Math.random() * 12) + 1;
-        crystalTwo = Math.floor(Math.random() * 12) + 1;
-        crystalThree = Math.floor(Math.random() * 12) + 1;
-        crystalFour = Math.floor(Math.random() * 12) + 1;
-        goalNumber = Math.floor(Math.random() * 101) + 19;
-        playerNumber = 0;
-        $("#player-score-var").text(playerNumber);
-        $("#goal-score-text").text(goalNumber);
+        winner();
     }
     else if(playerNumber > goalNumber) {
-        losses++
-        $("#losses-var").text(losses);
-        alert("you LOSE!");
-        crystalOne = Math.floor(Math.random() * 12) + 1;
-        crystalTwo = Math.floor(Math.random() * 12) + 1;
-        crystalThree = Math.floor(Math.random() * 12) + 1;
-        crystalFour = Math.floor(Math.random() * 12) + 1;
-        goalNumber = Math.floor(Math.random() * 101) + 19;
-        playerNumber = 0;
-        $("#player-score-var").text(playerNumber);
-        $("#goal-score-text").text(goalNumber);
+        loser();
     }
  })
  
@@ -111,30 +77,10 @@ $("#crystal-two").on("click" , function(){
     console.log(playerNumber)
     $("#player-score-var").text(playerNumber);
     if(playerNumber === goalNumber) {
-        wins++
-        $("#wins-var").text(wins);
-        alert("You WON!");
-        crystalOne = Math.floor(Math.random() * 12) + 1;
-        crystalTwo = Math.floor(Math.random() * 12) + 1;
-        crystalThree = Math.floor(Math.random() * 12) + 1;
-        crystalFour = Math.floor(Math.random() * 12) + 1;
-        goalNumber = Math.floor(Math.random() * 101) + 19;
-        playerNumber = 0;
-        $("#player-score-var").text(playerNumber);
-        $("#goal-score-text").text(goalNumber);
+        winner();
     }
     else if(playerNumber > goalNumber) {
-        losses++
-        $("#losses-var").text(losses);
-        alert("you LOSE!");
-        crystalOne = Math.floor(Math.random() * 12) + 1;
-        crystalTwo = Math.floor(Math.random() * 12) + 1;
-        crystalThree = Math.floor(Math.random() * 12) + 1;
-        crystalFour = Math.floor(Math.random() * 12) + 1;
-        goalNumber = Math.floor(Math.random() * 101) + 19;
-        playerNumber = 0;
-        $("#player-score-var").text(playerNumber);
-        $("#goal-score-text").text(goalNumber);
+        loser();
     }
  });
 
